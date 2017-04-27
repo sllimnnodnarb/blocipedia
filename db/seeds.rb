fc0076b9e02fc53e2c5adb7e51cf7587c4c8846e
Brandon Mills <sllimnodnarb@gmail.com>
2017-04-27 10:08:44 -0500

    adding all files associated with user-story-6-seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,21 @@
+5.times do
+  User.create!(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password(8)
+  )
+end
+users = User.all
 
+5.times do
+  Wiki.create!(
+    user: users.sample,
+    title: Faker::Name.name,
+    body: Faker::Company.bs
+  )
+end
+wikis = Wiki.all
+
+puts "#{User.count} users created"
+puts "#{Wiki.count} wikis created"
+puts "Seed finished"
+puts "Some seeds fell beside the road, and the birds came and ate them up..."
